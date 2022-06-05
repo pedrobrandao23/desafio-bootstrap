c2fe360132a5ec60f5249cd9c5ea67736fa0fc70
“Pedro <pedro_brandao2@hotmail.com>
2022-06-05 19:14:53 -0300

    tentativa de alerts no cadastro e exclusao de produto

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const loginAlertPlaceholder = document.getElementById('liveAlertPlaceholder')
 
 const alert = (message, type) => {
   const wrapper = document.createElement('div')
@@ -9,12 +9,63 @@ const alert = (message, type) => {
     '</div>'
   ].join('')
 
-  alertPlaceholder.append(wrapper)
+  loginAlertPlaceholder.append(wrapper)
 }
 
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
     alert('Parabéns, você foi logado', 'success')
+  })
+}
+
+// --------------
+// alert no cadastro
+
+const cadastroAlertPlaceholder = document.getElementById('cadastroAlertPlaceholder')
+
+const cadastroAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  cadastroAlertPlaceholder.append(wrapper)
+}
+
+const cadastroAlertTrigger = document.getElementById('cadastroAlert')
+if (cadastroAlertTrigger) {
+  cadastroAlertTrigger.addEventListener('click', () => {
+    alert('Produto cadastrado!', 'success')
+  })
+}
+
+// ----------------
+// alert no excluir
+
+const excluirAlertPlaceholder = document.getElementById('excluirAlertPlaceholder')
+
+const excluirAlert = (message, type) => {
+  const wrapper = document.createElement('div');
+
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+
+  excluirAlertPlaceholder.append(wrapper);
+};
+
+
+const excluirAlertTrigger = document.getElementById('excluirAlert')
+if (excluirAlertTrigger) {
+  excluirAlertTrigger.addEventListener('click', () => {
+    alert('Produto excluido', 'success')
   })
 }
